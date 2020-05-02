30c6d5546ca42c3241662311beaf0e813404cac0
Azis <azuolas.krusna@yahoo.com>
2020-05-02 21:01:18 +0300

    Add more tests

--- tests/test.py
+++ tests/test.py
@@ -25,6 +25,43 @@ class TestRegexes(unittest.TestCase):
         '''
         self.assertEqual(sql, sql_correct)
 
+    def test_is_not_null(self):
+        sql = app.read('''
+            selECT 1,2,3
+            where bounty is null
+        ''')
+        sql_correct = '''
+            SELECT 1,2,3
+            WHERE bounty IS NULL
+        '''
+        self.assertEqual(sql, sql_correct)
+
+    def test_distinct(self):
+        sql = app.read('''
+            select distinct stars
+            from universe
+        ''')
+        sql_correct = '''
+            SELECT DISTINCT stars
+            FROM universe
+        '''
+        self.assertEqual(sql, sql_correct)
+
+    def test_create_table_if_not_exists(self):
+        sql = app.read('''
+             Create table if not exists `tblsample` (
+                `id` VARCHAR(100) NOT NULL auto_increment,
+                `name` VARCHAR(120) NOT NULL default 'unknown'
+            )
+        ''')
+        sql_correct = '''
+             CREATE TABLE IF NOT EXISTS `tblsample` (
+                `id` VARCHAR(100) NOT NULL auto_increment,
+                `name` VARCHAR(120) NOT NULL default 'unknown'
+            )
+        '''
+        self.assertEqual(sql, sql_correct)
+
 
 if __name__ == '__main__':
     unittest.main()
