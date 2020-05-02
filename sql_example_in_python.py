d38541c68b1f942303c091bafb6bf5cf02159de9
Azis <azuolas.krusna@yahoo.com>
2020-05-02 21:18:29 +0300

    Add more SQL examples in python

--- sql_example_in_python.py
+++ sql_example_in_python.py
@@ -1,9 +1,25 @@
 query = """
-	select * from languages;
+    select * from languages;
 """
 
 query = """
-	select * 
-	from games 
-	where test=0
+    select * 
+    from games 
+    where test=0
 """
+
+
+def get_query():
+    limit = 6
+    query = f"SELEct max(weight) from world where ocean='Atlantic'"
+    return query
+
+
+def get_query():
+    query = f"SELEct speed from world where animal='dolphin' limit {limit}"
+    return query
+
+
+def return_5():
+    query = 'select 5'
+    return query
